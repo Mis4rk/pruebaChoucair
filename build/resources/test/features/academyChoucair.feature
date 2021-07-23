54302ebd1b39a0ed29fb2f504001f17103d5e5d8
Mis4rk <jpce666@gmail.com>
2021-07-22 21:46:22 -0500

    first commit

---
+++ build/resources/test/features/academyChoucair.feature
@@ -0,0 +1,8 @@
+@stories
+  Feature: Academy Choucair
+    As a user, i want to learn how to automate in screenplay at the Choucair Academy with the automation course
+  @scenario1
+  Scenario: Search for the automation course
+    Given than Brandom wants to learn automation at the academy Choucair
+    When he search for the course Recursos Automatizacion Bancolombia on the choucair academy platform
+    Then he finds the course called resources Recursos Automatizacion Bancolombia
---
+++ src/test/java/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -0,0 +1,30 @@
+package stepdefinitions;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import tasks.OpenUp;
+
+public class ChoucairAcademyStepDefinitions {
+    @Before
+    public void setStage(){
+        OnStage.setTheStage(new OnlineCast());
+    }
+    @Given("^than Brandom wants to learn automation at the academy Choucair$")
+    public void thanBrandomWantsToLearnAutomationAtTheAcademyChoucair() {
+        OnStage.theActorCalled("Brandom").wasAbleTo(OpenUp.thePage());
+    }
+
+    @When("^he search for the course Recursos Automatizacion Bancolombia on the choucair academy platform$")
+    public void heSearchForTheCourseRecursosAutomatizacionBancolombiaOnTheChoucairAcademyPlatform() {
+
+    }
+
+    @Then("^he finds the course called resources Recursos Automatizacion Bancolombia$")
+    public void heFindsTheCourseCalledResourcesRecursosAutomatizacionBancolombia() {
+
+    }
+}
